478999299fc14450c208975b4534e2d3207b73f3
tomoyk <tmyk.kym@gmail.com>
2018-02-24 14:21:58 +0900

    fixed templates

--- main.php
+++ main.php
@@ -6,8 +6,13 @@ $loader = new Twig_Loader_Filesystem('templates');
 $twig = new Twig_Environment($loader, [
   'debug' => true, 
   'auto_reload' => true, 
-  'strict_variables' => true
+  // 'strict_variables' => true
 ]);
 
-echo $twig->render('_base.html', ['title' => 'IPv4 Lease']);
+$pages = ['IPv4 Lease', 'IPv6 Lease', 'About'];
+
+echo $twig->render('_base.html', [
+  'navLabels' => $pages,
+  'enableItem' => 0,
+]);
 
