52cac4cae54fbe3c576c896d9aab2493b0bf6ab0
tomoyk <tmyk.kym@gmail.com>
2018-02-24 17:23:41 +0900

    fixed templates

--- main.php
+++ main.php
@@ -11,8 +11,18 @@ $twig = new Twig_Environment($loader, [
 
 $pages = ['IPv4 Lease', 'IPv6 Lease', 'About'];
 
-echo $twig->render('_base.html', [
-  'navLabels' => $pages,
-  'enableItem' => 0,
-]);
+if(isset($_GET['v4'])) {
+  
+  echo $twig->render('_ipTable.html', [
+    'navLabels' => $pages,
+    'enableItem' => 2
+  ]);
 
+} elseif(isset($_GET['v6'])) {
+
+  echo $twig->render('_base.html', [
+    'navLabels' => $pages,
+    'enableItem' => 0,
+  ]);
+
+}
