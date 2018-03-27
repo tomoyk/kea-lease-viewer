1b118f58f998b7cba274a5822133db8fba28b3bf
tomoyk <tmyk.kym@gmail.com>
2018-03-27 21:19:28 +0900

    fixed menu logic

--- main.php
+++ main.php
@@ -2,6 +2,7 @@
 
 require_once 'vendor/autoload.php';
 
+// Initialize Twig
 $loader = new Twig_Loader_Filesystem('templates');
 $twig = new Twig_Environment($loader, [
   'debug' => true, 
@@ -9,20 +10,29 @@ $twig = new Twig_Environment($loader, [
   // 'strict_variables' => true
 ]);
 
+// Menu settings
 $pages = ['IPv4 Lease', 'IPv6 Lease', 'About'];
+$pages = [
+  // Slug => Label
+  'v4'     => 'IPv4 Lease',
+  'v6'     => 'IPv6 Lease',
+  'about'  => 'About'      
+];
 
-if(isset($_GET['v4'])) {
+if(isset($_GET['v4']) || isset($_GET['v6'])) {
+
+  $active = isset($_GET['v4']) ? 'v4' : 'v6';
   
   echo $twig->render('_ipTable.html', [
     'navLabels' => $pages,
-    'enableItem' => 2
+    'enableItem' => $active
   ]);
 
-} elseif(isset($_GET['v6'])) {
+} elseif(isset($_GET['about'])){
 
   echo $twig->render('_base.html', [
     'navLabels' => $pages,
-    'enableItem' => 0,
+    'enableItem' => 'about'
   ]);
 
 }
